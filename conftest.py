31abb957bfd874e459b4ae90850d287946a3905a
Macloren <88139137+Macloren@users.noreply.github.com>
2022-03-25 10:38:46 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,26 +1,20 @@
-import os
-
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
 
 def pytest_addoption(parser):
-    parser.addoption("--language", default="en", help="Choose language")
+    parser.addoption('--language', action='store', default='en', help='Specify language. En by default')
 
-
-@pytest.fixture(scope="function")
+@pytest.fixture(scope='function')
 def browser(request):
-    language_to_use = request.config.getoption("language")
-    options = ChromeOptions()
-    options.add_argument(f"--lang={language_to_use}")
-
-    if os.getenv("TRAVIS") is not None:
-        options.add_argument("--no-sandbox")
-        options.add_argument("--headless")
-
+    language = request.config.getoption('language')
+    print('\nstarting browser for tests')
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
     browser = webdriver.Chrome(options=options)
-
     yield browser
-
+    print('\nquitting browser')
+    #time.sleep(60)
     browser.quit()
