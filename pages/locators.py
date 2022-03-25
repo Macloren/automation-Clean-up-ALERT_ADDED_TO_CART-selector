aaab48c1ef3cb1c5afca9ce45b1868e488dbf867
Macloren <88139137+Macloren@users.noreply.github.com>
2022-03-25 10:37:15 +0300

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -1,34 +1,30 @@
 from selenium.webdriver.common.by import By
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
-class BasePageLocators:
-    LOGIN_LINK = (By.ID, "login_link")
-    LOGIN_LINK_INVALID = (By.ID, "login_link_inv")
-    CART_BUTTON = (By.CSS_SELECTOR, ".basket-mini a.btn.btn-default")
-    USER_ICON = (By.CLASS_NAME, "icon-user")
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    VIEW_CART_BUTTON = (By.XPATH, '//a[@class="btn btn-default"]')
 
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
+    REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+    EMAIL_FIELD = (By.CSS_SELECTOR, '#id_registration-email.form-control')
+    PASS_FIELD = (By.CSS_SELECTOR, '#id_registration-password1.form-control')
+    CONFIRM_PASS_FIELD = (By.CSS_SELECTOR, '#id_registration-password2.form-control')
+    REGISTER_BUTTON = (By.CSS_SELECTOR, '[value=\'Register\']')
 
-class LoginPageLocators:
-    LOGIN_BUTTON = (By.ID, "login_link")
-    EMAIL_INPUT = (By.NAME, "registration-email")
-    PASSWORD_INPUT = (By.NAME, "registration-password1")
-    REPEAT_PASSWORD_INPUT = (By.NAME, "registration-password2")
-    REGISTER_BUTTON = (By.NAME, "registration_submit")
-    LOGIN_FORM = (By.ID, "login_form")
-    REGISTER_FORM = (By.ID, "register_form")
+class ProductPageLocators():
+    ADD_TO_CART = (By.CSS_SELECTOR, '.btn.btn-lg.btn-primary.btn-add-to-basket')
+    PRODUCT_TITLE = (By.CSS_SELECTOR, '.col-sm-6.product_main h1')
+    CONFIRMATION_TITLE = (By.CSS_SELECTOR, '.alertinner :first-child')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, 'p.price_color')
+    CART_PRICE = (By.CSS_SELECTOR, '.alertinner p strong')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, 'div.alert.alert-safe.alert-noicon.alert-success.fade.in:first-child')
 
-
-class ProductPageLocators:
-    BUTTON_ADD_TO_CART = (By.CLASS_NAME, "btn-add-to-basket")
-    ADDING_SUCCESS = (By.CSS_SELECTOR, "div.alert-success")
-    ALERT_ADDED_TO_CART = (By.CSS_SELECTOR,
-                           "#messages>div:first-child .alertinner strong")
-    ALERT_CART_STATUS = (By.CSS_SELECTOR, ".alert-noicon.alert-info p")
-    PRICE_VALUE = (By.CLASS_NAME, "price_color")
-    PRODUCT_NAME = (By.TAG_NAME, "h1")
-    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
-
-
-class CartPageLocators:
-    CART_ELEMENT = (By.CLASS_NAME, "basket-items")
-    BASKET_EMPTY_TEXT_ELEMENT = (By.CSS_SELECTOR, "div#content_inner > p")
+class CartPageLocators():
+    ADDED_ITEMS = (By.CSS_SELECTOR, '#basket_formset')
+    EMPTY_CART_NOTIFICATION = (By.CSS_SELECTOR, '#content_inner > p')
