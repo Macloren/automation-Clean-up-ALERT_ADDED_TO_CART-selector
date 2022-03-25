edce793c25b9f53af4894265bdc2085a062d49ee
Macloren <88139137+Macloren@users.noreply.github.com>
2022-03-25 10:37:31 +0300

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,35 +1,30 @@
-from .base_page import BasePage
-from .locators import LoginPageLocators
-
+from base_page import BasePage
+from locators import LoginPageLocators
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
 
 class LoginPage(BasePage):
-    def register_new_user(self, email: str, password: str) -> None:
-        self.browser.find_element(*LoginPageLocators.EMAIL_INPUT).send_keys(
-            email)
-        self.browser.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(
-            password)
-        self.browser.find_element(
-            *LoginPageLocators.REPEAT_PASSWORD_INPUT).send_keys(password)
-        self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
-
-    def should_be_login_page(self) -> None:
+    def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
 
-    def should_be_login_url(self) -> None:
-        # реализуйте проверку на корректный url адрес
-        current_url = self.browser.current_url
-        assert "login" in current_url, f"No login substring in the {current_url}"
+    def should_be_login_url(self):
+        assert 'login' in self.browser.current_url
 
-    def should_be_login_form(self) -> None:
-        # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(
-            *LoginPageLocators.LOGIN_FORM
-        ), f"Login form is not present on the {self.browser.current_url}"
+    def should_be_login_form(self):
+        assert self.browser(*LoginPageLocators.LOGIN_FORM), "Login forn was not located"
 
-    def should_be_register_form(self) -> None:
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(
-            *LoginPageLocators.REGISTER_FORM
-        ), f"Register form is not present on the {self.browser.current_url} page"
+    def should_be_register_form(self):
+        assert self.browser(*LoginPageLocators.REGISTER_FORM), 'Registration form was not located'
+
+    def register_new_user(self, email, password):
+        email_address = self.browser.find_element(*LoginPageLocators.EMAIL_FIELD)
+        email_address.send_keys(email)
+        pass_field = self.browser.find_element(*LoginPageLocators.PASS_FIELD)
+        pass_field.send_keys(password)
+        confirm_pass = self.browser.find_element(*LoginPageLocators.CONFIRM_PASS_FIELD)
+        confirm_pass.send_keys(password)
+        button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        button.click()
